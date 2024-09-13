7c3b63700c3bb2d5613b1d1d8df2473257f89adc
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-09-13 19:23:04 -0300

    feat: creating entities and adding repository to the user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,19 +4,19 @@
 import { Controller, Post, Body, Get } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UserService } from './user.service';
-import { User } from './interfaces/user.interface';
+import { UserEntity } from './interfaces/user.entity';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
+  createUser(@Body() createUserDto: CreateUserDto): Promise<UserEntity> {
       return this.userService.createUser(createUserDto);
   }
 
   @Get()
-  getAllUsers(): User[] {
+  getAllUsers(): Promise<UserEntity[]> {
       return this.userService.getAllUsers();
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,22 +1,43 @@
-/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dtos/createUser.dto'; 
-import { User } from './interfaces/user.interface';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { UserEntity } from './interfaces/user.entity';
 import * as bcrypt from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-    private users: User[] = [];
-    private idCounter = 1;
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
 
-    async createUser(createUserDto: CreateUserDto): Promise<User> {
-        const passwordHashed = await bcrypt.hash(createUserDto.password, 10);
-        const newUser: User = { ...createUserDto, password: passwordHashed, id: this.idCounter++ };
-        this.users.push(newUser);
-        return newUser;
-    }
+  async createUser(createUserDTO: CreateUserDto): Promise<UserEntity> {
+    try {
+      const saltOrRounds = 10;
+      const passwordHashed = await bcrypt.hash(
+        createUserDTO.password,
+        saltOrRounds,
+      );
 
-    getAllUsers(): User[] {
-        return this.users;
+      const existingUser = await this.userRepository.findOne({
+        where: { email: createUserDTO.email },
+      });
+      if (existingUser) {
+        throw new Error('User with this email already exists');
+      }
+
+      return this.userRepository.save({
+        ...createUserDTO,
+        password: passwordHashed,
+      });
+    } catch (error) {
+      console.error('Error creating user:', error);
+      throw new Error('Could not create user');
     }
+  }
+
+  async getAllUsers(): Promise<UserEntity[]> {
+    return this.userRepository.find();
+  }
 }
